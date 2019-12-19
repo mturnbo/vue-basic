a152ff4d8ac74c63e9b94a5ed13f82409d8cb302
Marcus Turnbo <marcus@emarcus.com>
2019-12-19 15:38:14 -0600

    Added vue app

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h2>{{ appName }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    el: '#app',
+    data: () => ({
+      appName: 'Vue Basic'
+    }),
+  }
+</script>
